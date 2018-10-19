efb9de631befa56b0380a25e6df6a0cf0b6fc2da
emmett1 <emmett1.2miligrams@gmail.com>
2018-10-19 23:20:06 +0800

    add mkinitramfs and init.in

---
+++ INSTALL.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+BINDIR=/usr/bin
+DATADIR=/usr/share/mkinitramfs
+
+mkdir -pv ${DESTDIR}{${BINDIR},${DATADIR}}
+install -m755 mkinitramfs ${DESTDIR}${BINDIR}
+install -m644 init.in ${DESTDIR}${DATADIR}
