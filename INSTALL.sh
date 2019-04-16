834e73e002a4f232650d3a9fb2df817692931d2a
emmett1 <emmett1.2miligrams@gmail.com>
2019-04-16 16:44:16 +0800

    add mkinitramfs.conf, add autodetect modules option

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,7 +2,9 @@
 
 BINDIR=/usr/bin
 DATADIR=/usr/share/mkinitramfs
+CONFDIR=/etc
 
-mkdir -pv ${DESTDIR}{${BINDIR},${DATADIR}}
+mkdir -pv ${DESTDIR}{${BINDIR},${DATADIR},${CONFDIR}}
 install -m755 mkinitramfs ${DESTDIR}${BINDIR}
 install -m644 init.in ${DESTDIR}${DATADIR}
+install -m644 mkinitramfs.conf ${DESTDIR}${CONFDIR}
