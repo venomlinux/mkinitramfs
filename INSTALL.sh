1509b53862ddf518e728890f01c281c613b7f06a
emmett1 <emmett1.2miligrams@gmail.com>
2019-06-23 16:17:15 +0800

    rewrite mkinitramfs script and add lsmkinitramfs script

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,5 +6,6 @@ CONFDIR=/etc
 
 mkdir -pv ${DESTDIR}{${BINDIR},${DATADIR},${CONFDIR}}
 install -m755 mkinitramfs ${DESTDIR}${BINDIR}
+install -m755 lsmkinitramfs ${DESTDIR}${BINDIR}
 install -m644 init.in ${DESTDIR}${DATADIR}
 install -m644 mkinitramfs.conf ${DESTDIR}${CONFDIR}
