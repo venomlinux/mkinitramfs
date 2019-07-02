d0645536c4a54c83431bb7bed50c480e36b7058a
emmett1 <emmett1.2miligrams@gmail.com>
2019-07-02 12:57:18 +0800

    updated

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,8 +3,9 @@
 BINDIR=/usr/bin
 DATADIR=/usr/share/mkinitramfs
 CONFDIR=/etc
+CONFDDIR=/etc/mkinitramfs.d
 
-mkdir -pv ${DESTDIR}{${BINDIR},${DATADIR},${CONFDIR}}
+mkdir -pv ${DESTDIR}{${BINDIR},${DATADIR},${CONFDIR},${CONFDDIR}}
 install -m755 mkinitramfs ${DESTDIR}${BINDIR}
 install -m755 lsmkinitramfs ${DESTDIR}${BINDIR}
 install -m644 init.in ${DESTDIR}${DATADIR}
