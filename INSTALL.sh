21cce772cabc17253f0ce2aed6d5c96c92cb86e6
emmett1 <emmett1.2miligrams@gmail.com>
2019-07-06 17:47:14 +0800

    update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,9 +4,11 @@ BINDIR=/usr/bin
 DATADIR=/usr/share/mkinitramfs
 CONFDIR=/etc
 CONFDDIR=/etc/mkinitramfs.d
+HOOKDIR=$DATADIR/hooks
 
-mkdir -pv ${DESTDIR}{${BINDIR},${DATADIR},${CONFDIR},${CONFDDIR}}
+mkdir -pv ${DESTDIR}{${BINDIR},${DATADIR},${CONFDIR},${CONFDDIR},${HOOKDIR}}
 install -m755 mkinitramfs ${DESTDIR}${BINDIR}
 install -m755 lsmkinitramfs ${DESTDIR}${BINDIR}
 install -m644 init.in ${DESTDIR}${DATADIR}
 install -m644 mkinitramfs.conf ${DESTDIR}${CONFDIR}
+install -m644 hook/* ${DESTDIR}${HOOKDIR}
